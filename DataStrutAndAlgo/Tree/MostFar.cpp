815b2ded2ee2b905ea433e8082e83233f60c3513
lian.pengfei <lian.pengfei@ztesoft.com>
2018-10-25 10:49:03 +0800

    ds
    ds

---
+++ DataStrutAndAlgo/Tree/MostFar.cpp
@@ -0,0 +1,14 @@
+/*
+ * MostFar.cpp
+ *
+ *  Created on: 2018Äê10ÔÂ25ÈÕ
+ *      Author: Administrator
+ */
+
+
+
+/*
+ *
+ * https://blog.csdn.net/liuyi1207164339/article/details/50898902
+ *
+ * */
---
+++ src/za/IntegralType.cpp
@@ -0,0 +1,38 @@
+/*
+ * IntegralType.cpp
+ *
+ *  Created on: 2018年10月25日
+ *      Author: Administrator
+ */
+
+
+
+/*
+ *                             最小尺寸
+ * bool     未定义
+ * char     8
+ * short   16
+ * int     16
+ * long    32
+ * long long   64
+ * float       6位有效数字
+ * double       10位有效数组
+ * long double  10位有效数组
+ * */
+
+#include <string>
+#include <vector>
+
+
+struct Cmp
+{
+std::string sIp;
+double   lVol;
+};
+
+
+std::string GetIp( std::vector<Cmp> cmps)
+{
+    std::string sRet;
+    return sRet;
+}
