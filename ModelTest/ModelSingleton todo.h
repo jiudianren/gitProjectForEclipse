4903e98bdc408405a50354a2590739f37367c73d
lian.pengfei <lian.pengfei@ztesoft.com>
2018-11-23 13:32:02 +0800

    ds
    as

---
+++ ModelTest/ModelSingleton todo.h
@@ -0,0 +1,16 @@
+/*
+ * ModelSingleton.h
+ *
+ *  Created on: 2018年3月15日
+ *      Author: Administrator
+ */
+
+#ifndef MODELTEST_MODELSINGLETON_TODO_H_
+#define MODELTEST_MODELSINGLETON_TODO_H_
+
+
+//todo  什么是懒汉模式，什么是饿汗模式 ，哪个是线程安全的？
+
+
+
+#endif /* MODELTEST_MODELSINGLETON_TODO_H_ */
