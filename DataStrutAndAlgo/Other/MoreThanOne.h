c79d493075749d8a4f19a522510d23e29da65483
jiudianren <648087012@qq.com>
2019-06-12 15:33:13 +0800

    Create MoreThanOne.h

---
+++ DataStrutAndAlgo/Other/MoreThanOne.h
@@ -0,0 +1,20 @@
+/*
+ * MoreThanOneSolution.h
+ *
+ *  Created on: 2019年6月12日
+ *      Author: 10259187
+ */
+
+#ifndef SRC_ALG_MORETHANONE_H_
+#define SRC_ALG_MORETHANONE_H_
+
+
+#include <vector>
+class  MoreThanOneSolution
+{
+public:
+	static void  TestCase();
+	int choose( const std::vector<int>  in);
+};
+
+#endif /* SRC_ALG_MORETHANONE_H_ */
